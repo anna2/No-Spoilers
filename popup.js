e745dba324deeb94d5db5a6f8974c1f7c760e55d
Anna Sheaffer <annasheaffer@gmail.com>
2014-03-17 14:10:12 -0400

    Pull out and name callbacks

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-function buildList() {
+// Read blockList from storage and create list of blocked elements
+// on the popup
+function buildPopupList() {
     var list = document.getElementById('list');
     chrome.storage.sync.get("blockList", function(data){
         var titles = data["blockList"] || [];
@@ -11,6 +13,7 @@ function buildList() {
     })
 }
 
+// Remove all LI elements from popup list
 function emptyList() {
     var list = document.getElementById('list');
     while(list.firstChild) {
@@ -18,28 +21,36 @@ function emptyList() {
     }
 }
 
+// Check whether extension is ON or OFF, then send message to 
+// inject appropriate content script on the current page. 
 function updateDOM() {
     chrome.storage.sync.get("toggle", function(data){
         if (data["toggle"]  == true) {
             chrome.runtime.sendMessage({method: "updateDOM"}, function(response){})
+        } else {
+            chrome.runtime.sendMessage({method: "off"}, function(response){})
         }
     })
 }
 
+// When user submits a new title to block, update stored blockList,
+// update popup list, rerun content script on current page.
 function addTitle() {
     chrome.storage.sync.get("blockList", function(data){
         var titles = data["blockList"] || [];
         var input = document.getElementById('title').value;
+        console.log("Inputs: " + input);
         titles.push(input);
         chrome.storage.sync.set({"blockList": titles}, function() {
+            console.log("Heere");
             emptyList();
-            buildList();
+            buildPopupList();
             updateDOM();
         })
     })
 }
 
-
+//Called when user removes title from popup list.
 function removeTitle(e) {
     if (e.target.id) {
         // remove node from popup DOM
@@ -48,33 +59,36 @@ function removeTitle(e) {
         node.remove();
 
         // remove title from master list
-        chrome.storage.sync.get("blockList", function(data){
-            var titles = data["blockList"] || [];
-            var index = titles.indexOf(nodeText);
-            titles.splice(index, 1);
-            chrome.storage.sync.set({"blockList": titles}, function() {
-
-
-                //update showList
-                chrome.storage.sync.get("showList", function(data) {
-                    var show = data["showList"] || [];
-                    show.push(nodeText);
-                    chrome.storage.sync.set({"showList": show}, function(){
-                        updateDOM();
-                    })
-                })
-            })
-        })
+        removeFromBlockList(nodeText);
     }
 }
 
-function off() {
-    console.log("In off");
-    chrome.runtime.sendMessage({method: "off"}, function(response){})
+//Remove title from stored blockList
+function removeFromBlockList(text) {
+    chrome.storage.sync.get("blockList", function(data){
+        var titles = data["blockList"] || [];
+        var index = titles.indexOf(text);
+        titles.splice(index, 1);
+        chrome.storage.sync.set({"blockList": titles}, function() {
+            addToShowList(text);             //update showList
+        })
+    })
+}
+
+//Place unblocked title on showList
+function addToShowList(text) {
+    chrome.storage.sync.get("showList", function(data) {
+        var show = data["showList"] || [];
+        show.push(text);
+        chrome.storage.sync.set({"showList": show}, function(){
+            updateDOM(); //when showList is saved, rerun content scripts
+        })
+    })
 }
 
 function checkBox() {
-    console.log("in checkbox");
+    //Read from storage whether extension is on or off
+    //and set checkbox accordingly.
     var input = document.getElementById('toggle');
     chrome.storage.sync.get("toggle", function(data){
         if (data["toggle"] == true) {
@@ -83,24 +97,20 @@ function checkBox() {
             input.checked = false;
         }
     })
-    input.addEventListener("change", function() {
-        console.log("change!");
-        chrome.storage.sync.set({"toggle": input.checked}, function() {
-            if (input.checked == true) {
-                updateDOM();
-                console.log("true!");
-            } else {
-                off();
-                console.log("false~");
 
-            }
+    //Listen for and save changes in ON/OFF status.
+    //Trigger apropriate content scripts.
+    input.addEventListener("change", function() {
+        chrome.storage.sync.set({"toggle": input.checked}, function() {
+            updateDOM();
         })
     })
 }
 
+//Add listeners to loading of popup.
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('add').addEventListener('click', addTitle);
     checkBox();
-    buildList();
+    buildPopupList();
     document.getElementById('list').addEventListener('click', removeTitle);
 });
